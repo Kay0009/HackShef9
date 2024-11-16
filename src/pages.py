5c230227683f7c7b7c182ba22154398084c3896e
Alex Armes <45469986+alexarmesz@users.noreply.github.com>
2024-11-16 18:04:16 +0000

    fixed requirements fr and also like deleted old files

--- src/pages.py
+++
@@ -1,9 +0,0 @@
-import index, dashboard
-
-homePage = st.Page(index.Index(), url_path="/", title="Home")
-dashboardPage = st.Page(dashboard.Dashboard(), url_path="/dashboard", title="Dashboard")
-
-pages = [
-    homePage,
-    dashboardPage
-]
