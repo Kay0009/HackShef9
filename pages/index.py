32b56349e058bb89b1eeb7959ff487f41968e5bb
Freddy Cansick <freddycansick@gmail.com>
2024-11-16 13:56:02 +0000

    Streamlit setup, venv

---
+++ pages/index.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Pump & Dump Partners")
+st.subheader("An investment advice bureau that starts with you.")
+
+st.header("Ready to Invest?")
+st.page_link("pages/dashboard.py", label="Get Started")
+
+st.header("Our Values")
+st.write("At Pump & Dump Partners, we believe that every investment should start and end with you.")
